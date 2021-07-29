749f3d3ca83972182825a8ce36f51643732bcd3b
yytxdy <310095841@qq.com>
2021-07-30 01:56:15 +0800

    hhh

--- springcloud-auth-server/src/main/java/server/AuthServerApplication.java
+++ springcloud-auth-server/src/main/java/server/AuthServerApplication.java
@@ -1,38 +1,23 @@
 package server;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
-import org.springframework.util.StringUtils;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
+@EnableResourceServer
 public class AuthServerApplication {
-    public static final String AUTHENTICATION_SCHEME_BEARER = "Bearer";
-    @Autowired
-    private ResourceServerTokenServices tokenServices;
-
     @GetMapping("/user")
-    public Principal user(HttpServletRequest request) {
-        String authorization = request.getHeader("authorization");
-        if (StringUtils.isEmpty(authorization)) {
-            return null;
-        }
-        if (!authorization.startsWith(AUTHENTICATION_SCHEME_BEARER)) {
-            return null;
-        }
-        OAuth2Authentication user = tokenServices.loadAuthentication(authorization.substring(7));
-        return user;
+    public Principal user(Principal principal) {
+        return principal;
     }
 
     public static void main(String[] args) {
--- springcloud-auth-server/src/main/java/server/config/OauthWebSecurity.java
+++ springcloud-auth-server/src/main/java/server/config/OauthWebSecurity.java
@@ -2,6 +2,7 @@ package server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@Order(1)
 public class OauthWebSecurity extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
